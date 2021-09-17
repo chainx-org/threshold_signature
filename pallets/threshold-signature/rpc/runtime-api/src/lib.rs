d2368052faf5b53e4cbddbe0d189478a1063eeaa
AAweidai <1253464613@qq.com>
2021-09-17 18:35:47 +0800

    Support mast script (#2)
    * Add rough exec script
    
    * Correct test
    
    * Fix benchmark
    
    * Rename variable
    
    * Remove generate address into off-chain
    
    * Fix clippy
    
    * Fix clippy
    
    * Format code

--- pallets/threshold-signature/rpc/runtime-api/src/lib.rs
+++ pallets/threshold-signature/rpc/runtime-api/src/lib.rs
@@ -1,9 +1,9 @@
 // ! Runtime API definition required by threshold_signature RPC extensions.
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::too_many_arguments, clippy::unnecessary_mut_passed)]
-pub use pallet_threshold_signature::primitive::{Message, Script, Signature};
-use sp_runtime::AccountId32;
-use sp_runtime::DispatchError;
+pub use pallet_threshold_signature::primitive::{Message, Pubkey, Signature};
+use sp_runtime::{AccountId32, DispatchError};
+use sp_std::vec::Vec;
 
 sp_api::decl_runtime_apis! {
     pub trait ThresholdSignatureApi
@@ -11,7 +11,8 @@ sp_api::decl_runtime_apis! {
         fn verify_threshold_signature(
             addr: AccountId32,
             signature: Signature,
-            script: Script,
+            pubkey: Pubkey,
+            control_block: Vec<Vec<u8>>,
             message: Message,
         ) -> Result<bool, DispatchError>;
     }
--- pallets/threshold-signature/rpc/src/lib.rs
+++ pallets/threshold-signature/rpc/src/lib.rs
@@ -1,12 +1,12 @@
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
 pub use pallet_threshold_signature_rpc_runtime_api::{
-    Message, Script, Signature, ThresholdSignatureApi as ThresholdSignatureRuntimeApi,
+    Message, Pubkey, Signature, ThresholdSignatureApi as ThresholdSignatureRuntimeApi,
 };
 use sp_api::{BlockId, BlockT, ProvideRuntimeApi};
 use sp_blockchain::HeaderBackend;
 use sp_runtime::AccountId32;
-use sp_std::{marker::PhantomData, sync::Arc};
+use sp_std::{marker::PhantomData, sync::Arc, vec::Vec};
 
 #[rpc]
 pub trait ThresholdSignatureApi<BlockHash> {
@@ -16,7 +16,8 @@ pub trait ThresholdSignatureApi<BlockHash> {
         &self,
         addr: AccountId32,
         signature: Signature,
-        script: Script,
+        pubkey: Pubkey,
+        control_block: Vec<Vec<u8>>,
         message: Message,
         at: Option<BlockHash>,
     ) -> Result<bool>;
@@ -48,7 +49,8 @@ where
         &self,
         addr: AccountId32,
         signature: Signature,
-        script: Script,
+        pubkey: Pubkey,
+        control_block: Vec<Vec<u8>>,
         message: Message,
         at: Option<<Block as BlockT>::Hash>,
     ) -> Result<bool> {
@@ -57,7 +59,7 @@ where
             // If the block hash is not supplied assume the best block.
             self.client.info().best_hash));
         Ok(api
-            .verify_threshold_signature(&at, addr, signature, script, message)
+            .verify_threshold_signature(&at, addr, signature, pubkey, control_block, message)
             .is_ok())
     }
 }
